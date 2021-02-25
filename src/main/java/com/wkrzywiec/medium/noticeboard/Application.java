e233ff20210292069388526ad2f2ff47d495fb05
William H. Salazar <wsalazar@live.com>
2021-02-25 11:41:53 -0800

    Update Application.java

--- src/main/java/com/wkrzywiec/medium/noticeboard/Application.java
+++ src/main/java/com/wkrzywiec/medium/noticeboard/Application.java
@@ -8,7 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class Application {
 
-	//comment 
+	//comment another comment
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
